093ba76205b0cb6ff9e1edd8b0b26b208913ac8f
6nikkyhood9 <6nikkyhood9@gmail.com>
2021-10-15 15:18:01 +0300

    second commit of repo

--- main.js
+++ main.js
@@ -1,19 +1,43 @@
-'use strict';
+ 'use strict'
+ 
+ const item = document.querySelector('.item');
+ const placeholders = document.querySelectorAll('.placeholder');
 
-const getRandomInt = max => Math.floor(Math.random() * max);
 
-function cardPlugin(num = 0) {
-    const cards = document.querySelectorAll('.card');
+ function dragstart(event) {
+     event.target.classList.add('hold');
+     setTimeout(() => event.target.classList.add('hide'));
+ }
 
-    const removeActive = () => cards.forEach(card => card.classList.remove('active'));
+ function dragend(event) {
+     event.target.classList.remove('hold', 'hide');
+ }
 
-    cards[num].classList.add('active');
+ item.addEventListener('dragstart', dragstart);
+ item.addEventListener('dragend', dragend);
 
-    for (const card of cards) {
-        card.addEventListener('click', () => {
-            removeActive();
-            card.classList.add('active');
-        });
-    }
-}
-cardPlugin(getRandomInt(5))
+ function dragover(event) {
+     event.preventDefault();
+ }
+
+ for (const placeholder of placeholders) {
+     placeholder.addEventListener('dragover', dragover);
+     placeholder.addEventListener('dragenter', dragenter);
+     placeholder.addEventListener('dragleave', dragleave);
+     placeholder.addEventListener('drop', dragdrop);
+ }
+
+
+
+ function dragenter(event) {
+     event.target.classList.add('hovered');
+ }
+
+ function dragleave(event) {
+     event.target.classList.remove('hovered');
+ }
+
+ function dragdrop(event) {
+     event.target.classList.remove('hovered');
+     event.target.append(item);
+ }
